ca3e5297cbff740b43d095528bc50450401a0836
Dmitry Naumov <komrad.nda@yandex.ru>
2024-04-17 22:42:15 +0300

    02 react кабзда как подробно компоненты часть 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,68 @@
 import React from 'react'
 import './App.css'
+import { render } from '@testing-library/react'
 
 // function declaration
 function App() {
+	console.log('App rendering')
 	// что то полезное делает, обязана вернуть JSX
 	return (
 		<div>
-			This is APP component
+			<AppTitle />
 			<Rating />
 			<Accordion />
+			<Rating />
 		</div>
 	)
 }
 
+function AppTitle() {
+	console.log('AppTitle rendering')
+	return <>This is APP component</>
+}
+
 function Rating() {
+	console.log('Rating rendering')
 	return (
-		<div>
+		<>
 			<Star />
 			<Star />
 			<Star />
-		</div>
+			<Star />
+			<Star />
+		</>
 	)
 }
 
 function Accordion() {
+	console.log('Accordion rendering')
 	return (
-		<div>
-			<h3>Меню</h3>
-			<ul>
-				<li>1</li>
-				<li>2</li>
-				<li>3</li>
-			</ul>
-		</div>
+		<>
+			<AccordionTitle />
+			<AccordionBody />
+		</>
 	)
 }
 
 function Star() {
+	console.log('Star rendering')
 	return <div>Star</div>
+}
+
+function AccordionTitle() {
+	console.log('AccordionTitle rendering')
+	return <h3>Меню</h3>
+}
+
+function AccordionBody() {
+	console.log('AccordionBody rendering')
+	return (
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
+	)
 }
 
 export default App
